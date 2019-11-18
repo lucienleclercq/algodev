ab067a2def63c37b8a92257b5c663dbf121ae936
lucien leclercq <leclercq.lucien18@gmail.com>
2019-11-18 20:30:09 +0100

    maj bataille naval

---
+++ src/main/java/org/algodev/jeux/batailleNavale/CaseNavale.java
@@ -0,0 +1,23 @@
+package org.algodev.jeux.batailleNavale;
+
+
+import org.algodev.jeux.Case;
+
+public class CaseNavale extends Case {
+    protected boolean etat;
+
+    //Initialise l'état de la case qui correspond a n'a pas été toucher
+    public CaseNavale (){
+        super();
+        etat = true;
+    }
+
+    //change l'état en a été toucher
+    public void setEtat() {
+        etat = false;
+    }
+
+    public boolean isEtat() {
+        return etat;
+    }
+}
